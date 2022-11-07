07cb13d3970e13231c4b801c6206e55f1a9d8c9e
sz7li <lshaozhong@gmail.com>
2022-11-07 00:34:39 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -578,7 +578,7 @@ def config_parser():
                         help='frequency of console printout and metric loggin')
     parser.add_argument("--i_img",     type=int, default=500, 
                         help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=10000, 
+    parser.add_argument("--i_weights", type=int, default=500, 
                         help='frequency of weight ckpt saving')
     parser.add_argument("--i_testset", type=int, default=50000, 
                         help='frequency of testset saving')
