c641601071b0a0f2f98915974b0a3209bf52af48
sz7li <lshaozhong@gmail.com>
2022-11-17 13:28:34 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -812,7 +812,7 @@ def train():
     # Short circuit if only rendering out from trained model
     if args.render_only:
         print('RENDER ONLY')
-        tree = svox.N3Tree.load("tree_iter_50000.npz", device=device)
+        tree = svox.N3Tree.load("tree_test/tree_iter_50000.npz", device=device)
         render_kwargs_test['tree'] = tree
         with torch.no_grad():
             if args.render_test:
