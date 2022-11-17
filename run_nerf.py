082e81321dad5a590039584c6573e085616c99f0
sz7li <lshaozhong@gmail.com>
2022-11-17 03:27:37 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -234,7 +234,7 @@ def create_tree(center, radius):
     tree = svox.N3Tree(data_dim=16, data_format="RGBA",
                   center=center, radius=radius,
                   N=2, device="cpu",
-                  init_refine=5, depth_limit=10,
+                  init_refine=0, depth_limit=10,
                   extra_data=None)
     # tree.to("cuda")
     # for i in range(8):
@@ -789,6 +789,8 @@ def train():
     radius = np.sqrt(np.sum((bbox[0] - bbox[1]) ** 2) / 2) / 2
     # Create tree model
     tree = create_tree(center, radius)
+    for i in range(5):
+        tree.refine()
     print("Tree created with size ", len(tree.values))
     # tree = tree.load("tree_iter_9725.npz")
     
