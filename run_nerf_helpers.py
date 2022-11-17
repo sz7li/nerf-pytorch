9a63e123025211cd3093c54d99ef3e8c9905c52a
sz7li <lshaozhong@gmail.com>
2022-11-17 00:08:46 -0500

    log

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -76,7 +76,7 @@ def get_embedder(multires, i=0):
 
 # Model
 class NeRF(nn.Module):
-    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
+    def __init__(self, D=8, W=256, input_ch=16, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
         """ 
         """
         super(NeRF, self).__init__()
