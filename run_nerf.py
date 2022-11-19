87489b162ac303f4a1c7bfcd680fc04aa18c19bc
sz7li <lshaozhong@gmail.com>
2022-11-19 17:08:55 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -907,10 +907,10 @@ def train():
 
             sigma_thresh = -np.log(1.0 - alpha_thresh) / approx_delta
             print("Raw densities from tree values: ")
-            print(raw_densities.shape, raw_densities)
+            print(raw_densities.shape, raw_densities.device, raw_densities)
             prev = len(tree.values)
             # mask = raw_densities > sigma_thresh # in the order of tree.values 
-            mask = torch.randint(2, size=(1, len(raw_densities[0])), device='cpu')
+            mask = torch.randint(2, size=(1, len(raw_densities[0])), device=device)
             where = torch.where(raw_densities > sigma_thresh)[0]
             print(mask, mask.device)
             print(tree._all_leaves(), tree._all_leaves().device)
