9a2c14c5ee2fb7068f6ed029f2ed7d77100e4156
sz7li <lshaozhong@gmail.com>
2022-11-19 17:12:57 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -917,7 +917,8 @@ def train():
             print(tree._all_leaves(), tree._all_leaves().device)
             sel = tree._all_leaves()[mask].T
             print(sel, sel.device)
-            
+            print(sel.to(device))
+            print(sel, sel.device)
             # print(sel.device, device, tree.values.device)
 
             tree.refine(sel=(*sel, ))
