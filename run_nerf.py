4a8d41535dda7c3cb248c891feea29fdf438a595
sz7li <lshaozhong@gmail.com>
2022-11-19 19:01:51 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -912,7 +912,7 @@ def train():
             print("Leaves above sigma threshold: ", sigma_thresh, mask)
             print(raw_densities[mask])
             print("Tree refined from ", prev, len(tree.values))
-            print(optimizer.param_groups[0][-1])
+            print(optimizer.param_groups[0])
             return
             # if len(tree.values) > prev:
                 
