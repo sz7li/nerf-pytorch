f164870a8efdd13516cdcd5e36a32edc8f5411c2
sz7li <lshaozhong@gmail.com>
2022-11-27 20:10:51 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -528,7 +528,7 @@ def render_rays(ray_batch,
     # new_pts = get_features_from_rays(pts)
     raw = network_query_fn(features_at_intersections,viewdirs, network_fn)
     print(corners[500])
-    print(pts[500])
+    print(pts.shape)
     '''
     network_query_fn = lambda inputs, viewdirs, network_fn : run_network(inputs, viewdirs, network_fn,
                                                                 embed_fn=embed_fn,
