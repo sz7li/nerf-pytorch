556a275b63dbe277208a64656470646705a2e5d0
sz7li <lshaozhong@gmail.com>
2022-11-30 02:00:12 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -1188,7 +1188,7 @@ def train():
             torch.save({
                 'global_step': global_step,
                 'network_fn_state_dict': render_kwargs_train['network_fn'].state_dict(),
-                'network_fine_state_dict': render_kwargs_train['network_fine'].state_dict(),
+                # 'network_fine_state_dict': render_kwargs_train['network_fine'].state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
             }, path)
             print('Saved checkpoints at', path)
