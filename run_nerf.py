44fd3da2f1ff99917ce8447f32cd4d5a39cb372a
sz7li <lshaozhong@gmail.com>
2022-12-10 01:05:53 -0500

    log

--- run_nerf.py
+++ run_nerf.py
@@ -600,6 +600,7 @@ def render_rays(ray_batch,
         delta_t = (subcube_tmax - subcube_tmin) * cube_sz + step_size
         print("delta_t", delta_t, delta_t.shape)
         att = torch.exp(-F.relu(raw[..., -1]) * delta_t)
+        print(att.shape)
         weight = light_intensity[good_indices] * (1.0 - att)
         print(weight.shape)
 
